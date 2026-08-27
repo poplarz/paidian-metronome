7ef1a3d6fa6467e503456b8baa7fd110247faf34
poplarz <76496820+poplarz@users.noreply.github.com>
2026-08-27 21:12:17 +0800

    Add half and whole note beat units

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='paidian-github-v1';
+const CACHE='paidian-github-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
